2e04b5188e6b91e6355d46ae15458563897e0a38
muhammad moiz <muhammadmoiz367@gmail.com>
2020-04-16 14:25:45 +0500

    Updates

---
+++ precache-manifest.3d38a57198f52cfb75a47e95507df4b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "196ecf3b02d519e69869fadc163905c2",
+    "url": "/rent-a-car-app-react/index.html"
+  },
+  {
+    "revision": "14c40e6f5140f0601cf4",
+    "url": "/rent-a-car-app-react/static/css/main.0ed88181.chunk.css"
+  },
+  {
+    "revision": "f8477311aea3f47681e3",
+    "url": "/rent-a-car-app-react/static/js/2.56a1f598.chunk.js"
+  },
+  {
+    "revision": "93e5c6625923831b9de72724fafaaad1",
+    "url": "/rent-a-car-app-react/static/js/2.56a1f598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c40e6f5140f0601cf4",
+    "url": "/rent-a-car-app-react/static/js/main.dff33008.chunk.js"
+  },
+  {
+    "revision": "1bbba3b677f52ad2627f",
+    "url": "/rent-a-car-app-react/static/js/runtime-main.d70086b6.js"
+  }
+]);
